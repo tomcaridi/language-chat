52d14ea37c5a93b8bdf613b2306cb16280acbf4e
Tom Caridi <tom.caridi@gmail.com>
2015-12-15 15:25:15 -0500

    added conversations & messsages models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :natives
   has_many :native_languages, through: :natives, source: :language
 
   has_many :desireds
   has_many :desired_languages, through: :desireds, source: :language
+
+  has_many :converations, foreign_key: :sender_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214195437) do
+ActiveRecord::Schema.define(version: 20151215201759) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "desireds", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,6 +32,17 @@ ActiveRecord::Schema.define(version: 20151214195437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "natives", force: :cascade do |t|
     t.integer  "user_id"
