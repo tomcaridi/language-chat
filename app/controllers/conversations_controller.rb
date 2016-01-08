e811014254e451a0d862394b7f942b684915218b
Tom Caridi <tom.caridi@gmail.com>
2016-01-08 01:17:13 -0500

    previous chats rendering properly on conversation show page along with matched users and previous chat list

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -16,6 +16,10 @@ class ConversationsController < ApplicationController
 	  @receiver = interlocutor(@conversation)
 	  @messages = @conversation.messages
 	  @message = Message.new
+	  desired_languages = current_user.desired_languages
+	  native_languages = current_user.native_languages
+	  @users = User.where(id: Native.select(:user_id).where(language: desired_languages)).where(id: Desired.select(:user_id).where(language: native_languages))
+	  @conversations = Conversation.involving(current_user)
 	end
 
 	private
