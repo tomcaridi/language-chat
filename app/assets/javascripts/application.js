856e6e4dac95d056c87cce2bc6cddde4ca524484
Tom Caridi <tom.caridi@gmail.com>
2016-01-25 12:51:02 -0500

    check_box_tag for language choices broken after implementing Materialize CSS, still need to fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require private_pub
 //= require turbolinks
 //= require_tree .
-$(document).ready(function(){
+  $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('select').material_select();
